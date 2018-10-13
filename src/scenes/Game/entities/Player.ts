61291b2a2ad7272c6a78329678c98acd29dcc59d
Nik Sudan <nik@forge.uk>
2018-10-13 17:01:05 +0100

    format because ben doesnt like prettier

--- src/scenes/Game/entities/Player.ts
+++ src/scenes/Game/entities/Player.ts
@@ -15,147 +15,150 @@ interface IPosition {
 }
 
 export default class Player extends IEntity {
-    physicsBody: Body;
-    composite: Composite;
-    limbs: IEntity[];
-    image: HTMLImageElement;
-    buildUp: number = 0.0;
-    head: PlayerHead;
+  physicsBody: Body;
+  composite: Composite;
+  limbs: IEntity[];
+  image: HTMLImageElement;
+  buildUp: number = 0.0;
+  head: PlayerHead;
 
-    async initialize(x: number, y: number) {
-        this.image = await this.imageLoader.loadImage('/assets/images/playerBody.png');
+  async initialize(x: number, y: number) {
+    this.image = await this.imageLoader.loadImage(
+      '/assets/images/playerBody.png',
+    );
 
-        this.physicsBody = Bodies.rectangle(x, y, this.image.width, this.image.height, {
-          collisionFilter: {
-            category: 2,
-            mask: 0,
-          } as any,
-        });
+    this.physicsBody = Bodies.rectangle(
+      x,
+      y,
+      this.image.width,
+      this.image.height,
+      {
+        collisionFilter: {
+          category: 2,
+          mask: 0,
+        } as any,
+      },
+    );
 
-        this.physicsBody.force.y = 0;
+    this.physicsBody.force.y = 0;
 
-        const createLimb = async (LimbClass, offSet: IPosition) => {
-            const entity = await new LimbClass().initialize(x + offSet.x, y + offSet.y);
+    const createLimb = async (LimbClass, offSet: IPosition) => {
+      const entity = await new LimbClass().initialize(
+        x + offSet.x,
+        y + offSet.y,
+      );
 
-            const constraint = Constraint.create({
-                bodyA: this.physicsBody,
-                bodyB: entity.physicsBody,
-                stiffness: 0.6,
-                pointA: {
-                    x: offSet.x,
-                    y: offSet.y,
-                },
-                
-            });
+      const constraint = Constraint.create({
+        bodyA: this.physicsBody,
+        bodyB: entity.physicsBody,
+        stiffness: 0.6,
+        pointA: {
+          x: offSet.x,
+          y: offSet.y,
+        },
+      });
 
-            return {
-                entity,
-                constraint,
-            };
-        }
+      return {
+        entity,
+        constraint,
+      };
+    };
 
-        const head = await createLimb(PlayerHead, { x: 0, y: -40 });
-        this.head = head.entity;
-        const leftArm = await createLimb(PlayerArmLeft,
-          { x: -44, y: -34 },
-        );
-        const rightArm = await createLimb(PlayerArmRight, { x: +44, y: -34 });
-        const leftLeg = await createLimb(PlayerLegLeft,
-          { x: -38, y: 46 },
-        );
-        const rightLeg = await createLimb(PlayerLegRight,
-          { x: 38, y: 46 },
-        );
+    const head = await createLimb(PlayerHead, { x: 0, y: -40 });
+    this.head = head.entity;
+    const leftArm = await createLimb(PlayerArmLeft, { x: -44, y: -34 });
+    const rightArm = await createLimb(PlayerArmRight, { x: +44, y: -34 });
+    const leftLeg = await createLimb(PlayerLegLeft, { x: -38, y: 46 });
+    const rightLeg = await createLimb(PlayerLegRight, { x: 38, y: 46 });
 
-        this.composite = Composite.create({
-            bodies: [
-                this.physicsBody,
-                leftArm.entity.physicsBody,
-                rightArm.entity.physicsBody,
-                head.entity.physicsBody,
-                leftLeg.entity.physicsBody,
-                rightLeg.entity.physicsBody,
-            ],
-            constraints: [
-                leftArm.constraint,
-                rightArm.constraint,
-                head.constraint,
-                leftLeg.constraint,
-                rightLeg.constraint,
-            ],
-        });
+    this.composite = Composite.create({
+      bodies: [
+        this.physicsBody,
+        leftArm.entity.physicsBody,
+        rightArm.entity.physicsBody,
+        head.entity.physicsBody,
+        leftLeg.entity.physicsBody,
+        rightLeg.entity.physicsBody,
+      ],
+      constraints: [
+        leftArm.constraint,
+        rightArm.constraint,
+        head.constraint,
+        leftLeg.constraint,
+        rightLeg.constraint,
+      ],
+    });
 
-        this.limbs = [
-          head.entity,
-          leftArm.entity,
-          rightArm.entity,
-          leftLeg.entity,
-          rightLeg.entity,
-        ];
-        
-        return this;
+    this.limbs = [
+      head.entity,
+      leftArm.entity,
+      rightArm.entity,
+      leftLeg.entity,
+      rightLeg.entity,
+    ];
+
+    return this;
+  }
+
+  addToWorld(world: World) {
+    World.add(world, this.composite);
+  }
+
+  update(gameState: IGameState) {
+    const ROTATION_SPEED = 0.02;
+    const SPEED = 0.1;
+    const BUILD_UP_SPEED = 0.025;
+
+    if (gameState.keyPresses['ArrowLeft']) {
+      Body.setAngularVelocity(this.physicsBody, -ROTATION_SPEED);
     }
 
-    addToWorld(world: World) {
-        World.add(world, this.composite);
+    if (gameState.keyPresses['ArrowRight']) {
+      Body.setAngularVelocity(this.physicsBody, ROTATION_SPEED);
     }
 
-    update(gameState: IGameState) {
-      const ROTATION_SPEED = 0.02;
-      const SPEED = 0.1;
-      const BUILD_UP_SPEED = 0.025;
-
-      if (gameState.keyPresses['ArrowLeft']) {
-        Body.setAngularVelocity(this.physicsBody, -ROTATION_SPEED);
+    if (gameState.keyPresses['ArrowUp']) {
+      if (this.buildUp < 1.0) {
+        this.buildUp += BUILD_UP_SPEED;
       }
 
-      if (gameState.keyPresses['ArrowRight']) {
-        Body.setAngularVelocity(this.physicsBody, ROTATION_SPEED);
-      }
+      this.head.setFace('CHARGING');
+    } else {
+      if (this.buildUp > 0) {
+        const angle = this.physicsBody.angle - Math.PI / 2;
 
-      if (gameState.keyPresses['ArrowUp']) {
-        if (this.buildUp < 1.0) {
-          this.buildUp += BUILD_UP_SPEED;
+        if (this.buildUp > 0.5) {
+          Body.applyForce(this.physicsBody, this.physicsBody.position, {
+            x: Math.cos(angle) * (SPEED * this.buildUp),
+            y: Math.sin(angle) * (SPEED * this.buildUp),
+          });
         }
 
-        this.head.setFace('CHARGING');
+        this.buildUp = 0;
+      }
+      if (this.physicsBody.speed > 1) {
+        this.head.setFace('POOP');
       } else {
-
-        if (this.buildUp > 0) {
-          const angle = this.physicsBody.angle - Math.PI / 2;
-
-          if (this.buildUp > 0.5) {
-            Body.applyForce(this.physicsBody, this.physicsBody.position, {
-              x: Math.cos(angle) * (SPEED * this.buildUp),
-              y: Math.sin(angle) * (SPEED * this.buildUp),
-            });
-          }
-
-          this.buildUp = 0;
-        }
-        if (this.physicsBody.speed > 1) {
-          this.head.setFace('POOP');
-        } else {
-          this.head.setFace('PASSIVE');
-        }
+        this.head.setFace('PASSIVE');
       }
     }
+  }
 
-    render(context: CanvasRenderingContext2D) {
-        context.save();
-        context.translate(this.physicsBody.position.x, this.physicsBody.position.y);
-        context.rotate(this.physicsBody.angle);
+  render(context: CanvasRenderingContext2D) {
+    context.save();
+    context.translate(this.physicsBody.position.x, this.physicsBody.position.y);
+    context.rotate(this.physicsBody.angle);
 
-        context.drawImage(
-          this.image,
-          -this.image.width / 2,
-          -this.image.height / 2,
-        );
+    context.drawImage(
+      this.image,
+      -this.image.width / 2,
+      -this.image.height / 2,
+    );
 
-        context.restore();
+    context.restore();
 
-        for (let limb of this.limbs) {
-          limb.render(context);
-        }
+    for (let limb of this.limbs) {
+      limb.render(context);
     }
+  }
 }
