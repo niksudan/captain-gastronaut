91efb76ba1acae52bfe892fcf8085ef7c1759ba5
Rupert Ben Wiser <ben@benwiser.com>
2018-10-14 14:56:21 +0100

    Update start screen to go to intro screen

---
+++ src/scenes/IntroScreen/index.ts
@@ -0,0 +1,27 @@
+import IEntity from "../../definitions/IEntity";
+import ImageLoader from "../../loaders/ImageLoader";
+import { IGameState } from "../../definitions/IGameState";
+import { IScene } from "../../definitions/IScene";
+import { World } from "matter-js";
+import Game from "../Game";
+
+export default class IntroScreen implements IScene {
+  entities: IEntity[] = [];
+  entitiesToDestroy: string[] = [];
+  particlesToAdd: IEntity[] = [];
+  imageLoader: ImageLoader = new ImageLoader();
+
+  async initialize(gameState: IGameState) {
+
+    return true;
+  }
+
+  async update(world: World, gameState: IGameState) {
+    // Use me nik 😘
+    // gameState.setScene(new Game());
+  }
+
+  render(context: CanvasRenderingContext2D) {
+
+  }
+}
